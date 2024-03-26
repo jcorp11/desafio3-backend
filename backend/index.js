6e74eeeb9abed0be5682f5f282b7ac3c4635aac0
jcorp11 <joaco.011@gmail.com>
2024-03-26 11:37:08 -0300

    status codes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { postModel } from "./models/like.model.js";
 import postRoutes from "./routes/postRoutes.js";
 
 const PORT = process.env.PORT || 3500;
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -4,28 +4,53 @@ import { postModel } from "../models/like.model.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const posts = await postModel.findAll();
-  return res.status(200).json(posts);
+  try {
+    const posts = await postModel.findAll();
+    return res.status(200).json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 router.post("/", async (req, res) => {
   //   console.log(req.body);
   const { titulo, img, descripcion } = req.body;
-  const post = { titulo, img, descripcion };
-  await postModel.create(post);
-  return res.status(201).json(post);
+  if (!titulo || !img) {
+    return res.status(400).json({ message: "Missing data" });
+  }
+  const newPost = { titulo, img, descripcion };
+  try {
+    await postModel.create(newPost);
+    return res.status(201).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 router.put("/like/:id", async (req, res) => {
   const id = req.params.id;
-  const post = await postModel.like(id);
-  return res.status(200).json(post);
+  try {
+    const post = await postModel.like(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
-  const post = await postModel.remove(id);
-  return res.status(200).json(post);
+  try {
+    const post = await postModel.remove(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
